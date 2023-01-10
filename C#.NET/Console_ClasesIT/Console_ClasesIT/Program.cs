ead6109af83b0e2bde521b93c012b628762e21c9
Daniel Caro A <danibancaro@gmail.com>
2023-01-10 14:29:28 -0300

    10mo Commint, tercer punto terminado y corregido del Chackpoint de C#

---
+++ C#.NET/Console_ClasesIT/Console_ClasesIT/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ClasesIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Declarar dos variables n1 = 5 y n2 = 10.
+             Utilizando concatenación entre las variables y los literales, mostrar en pantalla la siguiente expresión:
+              n1 es igual a 5, n2 es igual a 10 y n1 más n2 es
+             igual a 15.*/
+
+            int n1 = 5;
+            int n2 = 10;
+            int n3 = n1 + n2;
+            Console.WriteLine("n1 es igual a " + n1 + ", n2 es igual a " + n2 + " y n1 más n2 es igual a " + n3);
+
+
+            /*Haciendo uso de la constante IVA = 21, calcular el precio con IVA de los siguientes productos e informar:
+             a.Remera: 59.90$
+             b.Pantalón: 99.90$
+             c.Campera: 149.90$
+            */
+
+            double remera = 59.99;
+            double pantalon = 99.90;
+            double campera = 149.90;
+            const double iva = 1.21;
+
+            double remera_iva = remera * iva;
+            double pantalon_iva = pantalon * iva;
+            double campera_iva = campera * iva;
+
+            Console.WriteLine("El precio del remera con iva es de $ {0}", remera_iva);
+            Console.WriteLine("El precio del pantalon con iva es de $ {0}", pantalon_iva);
+            Console.WriteLine("El precio del campera con iva es de $ {0}", campera_iva);
+
+
+
+            ////swicth
+            //string Nombre = "";
+            //int numeroOpcionCase = 0;
+            //Console.WriteLine("Ingrese un nombre de no más de 10 caracteres: ");
+            //Nombre = Console.ReadLine();
+
+            ////if (Nombre.Length < 11)
+            ////{
+            ////    Console.WriteLine("Nombre ingresado correctamente {0}", Nombre);
+            ////}
+
+            //// En el ejemplo, Nombre puede tomar tres valores distintos: "jose", "pablo", "maria"
+
+            //switch (Nombre)
+            //{
+            //    case "jose": Console.WriteLine("Nombre ingresado correctamente jose"); break;
+            //    case "pablo": Console.WriteLine("Nombre ingresado correctamente pablo"); break;
+            //    case "maria": Console.WriteLine("Nombre ingresado correctamente maria"); break;
+            //    default:
+            //        Console.WriteLine("Nombre distinto a los tres anteriores"); break;
+            //}
+
+
+            //Console.ReadKey(); deja la consola 
+
+            
+
+            string Nombre = "";
+            int numeroOpcionCase = 0;
+            Console.WriteLine("Ingrese opcion numero entero: ");
+            numeroOpcionCase = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (numeroOpcionCase)
+            {
+                case 1: Console.WriteLine("Nombre ingresado correctamente jose"); break;
+                case 2: Console.WriteLine("Nombre ingresado correctamente pablo"); break;
+                case 3: Console.WriteLine("Nombre ingresado correctamente maria"); break;
+                default:
+                    Console.WriteLine("Nombre distinto a los tres anteriores"); break;
+            }
+            Console.ReadKey();
+
+
+
+            int notaAlumno = Convert.ToInt32(Console.ReadLine());
+
+            if ((notaAlumno >= 4) & (notaAlumno < 7))
+            {
+                Console.WriteLine("El alumno ha aprobado.");
+            }
+            else if(notaAlumno >= 7 & notaAlumno <= 10)
+            {
+                Console.WriteLine("El alumno ha promocionado.");
+            }
+            else
+            {
+                Console.WriteLine("El alumno debe recuperar.");
+            }
+
+
+
+
+        }
+    }
+}
